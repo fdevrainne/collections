01bb8618b0683c9ac6fb071f35ce2bdc4ec3c000
SamuelRitchie <samuel.ritchie@ensae.fr>
2020-04-08 20:43:16 +0200

    update requirements & enable server access

--- discogs/discogs/settings.py
+++ discogs/discogs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zhn@g5&a$=t*)s)eh!g%gfnoezsjq(iw(s#-@9j+4k9=^r6+t8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mod_wsgi.server',
 ]
 
 MIDDLEWARE = [
